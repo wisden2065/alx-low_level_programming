de36969e02f254e4b5f10d36ee0dd3d0b40b80c5
Nnanyereugo <nnajiwisdom1998@gmail.com>
2022-09-16 13:16:48 -0700

    my new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-#include <unistd.h>
-
-=======
 #include <stdio.h>
 #include <unistd.h>
->>>>>>> b55b0d73a75321aa0a9ba600ae7aec14be9793ea
+
 /**
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
- *    *
- *     * Return: Always 1.
- *      */
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-<<<<<<< HEAD
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  59);
-
-		return (1);
-=======
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
->>>>>>> b55b0d73a75321aa0a9ba600ae7aec14be9793ea
+	write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
